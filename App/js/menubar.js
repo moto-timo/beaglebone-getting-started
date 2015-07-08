1fb880ed5e2b5d735c60af4e7e2a46727f674519
Ariane Paola Gomes <arianepaola@users.noreply.github.com>
2015-07-08 03:48:44 +0200

    Added ssh and scp connection functionality to menus

--- App/js/menubar.js
+++ App/js/menubar.js
@@ -1,23 +1,50 @@
 var gui = require('nw.gui');
 
+var appPath = process.cwd();
+
+console.log("app path: " + appPath);
+
 var win = gui.Window.get();
 var menubar = new gui.Menu({
     type: 'menubar'
 });
 
+var start = new gui.Menu();
 var connect = new gui.Menu();
 var about = new gui.Menu();
+
+start.append(new gui.MenuItem({
+    label: 'Start',
+    click: function() {
+        window.location.replace(appPath + '/html/en/index.html');
+    }
+}));
 
 connect.append(new gui.MenuItem({
     label: 'Open SSH Connection',
     click: function() {
+        var cp = require('child_process');
+        var child = cp.fork(appPath + '/js/ssh_server.js');
+
+        child.on('message', function(message) {
+            console.log('received: ' + message);
+        });
+
+        window.location.replace(appPath + '/html/en/terminal.html');
+    }
+}));
+
+connect.append(new gui.MenuItem({
+    label: 'SCP File Copy',
+    click: function() {
+        window.location.replace(appPath + '/html/en/copyfile.html');
     }
 }));
 
 about.append(new gui.MenuItem({
     label: 'Open Documentation',
     click: function() {
-        gui.Window.open('../README.htm');
+        gui.Window.open(appPath + '/README.htm');
     }
 }));
 
@@ -35,6 +62,7 @@ about.append(new gui.MenuItem({
     }
 }));
 
+menubar.append(new gui.MenuItem({label: 'Start', submenu: start}));
 menubar.append(new gui.MenuItem({label: 'Connect', submenu: connect}));
 menubar.append(new gui.MenuItem({label: 'Help', submenu: about}));
 
